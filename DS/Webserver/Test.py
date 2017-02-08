eba1e40798ecbf80831a5cc4d969da952ba32990
Gabe <gstroe@ltu.edu>
2017-02-08 15:49:58 -0500

    CS folder

--- DS/Webserver/Test.py
+++ DS/Webserver/Test.py
@@ -1,14 +1,19 @@
 # Headers
 from flask import Flask
+app = Flask(__name__) # The referce for flask to be the application that runs in this file
 
-app.route('/') # the base route to load the start of the web page
+@app.route('/') # the base route to load the start of the web page
 def index():
     return "Hello this is a test" # Quick test to see if it loads the txt
 	
 
-app.route('/<str:input>')
+@app.route('/<string:input>') # loads any input thats a part of the url
 def userInput(input):
 	return "Your input is: " + input
 	
+	
+if __name__ == '__main__':
+	app.run(host='0.0.0.0')
+
 
  
